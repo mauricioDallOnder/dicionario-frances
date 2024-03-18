96c5750e427ed6f7bc632afdea819d0d5b7e50e3
Mauricio Dall Onder <londer11@icloud.com>
2024-03-17 23:20:40 -0300

    Update page.tsx

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableRow, Typography, Button } from '@mui/material';
