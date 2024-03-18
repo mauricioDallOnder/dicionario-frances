d9c10dfd2e353a82b0065137912662bdee7d15a1
Mauricio Dall Onder <londer11@icloud.com>
2024-03-17 23:40:19 -0300

    Create page.tsx

---
+++ app/history/page.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableRow, Typography, Button } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+
+type HistoryType = Record<string, string>;
+type HistoryComponentProps = {
+  onSelectWord: (word: string, translation: string) => void;
+};
+
+export default function HistoryComponent({ onSelectWord }: HistoryComponentProps) {
+  const [history, setHistory] = useState<HistoryType>({});
+
+  useEffect(() => {
+    fetchHistory();
+  }, []);
+
+  const fetchHistory = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:5000/api/history');
+      setHistory(response.data);
+    } catch (error) {
+      console.error('Erro ao buscar o histórico:', error);
+    }
+  };
+
+  const handleDeleteWord = async (word: string) => {
+    try {
+      await axios.delete(`http://127.0.0.1:5000/api/history/${word}`);
+      fetchHistory(); // Atualizar o histórico após deletar a palavra
+    } catch (error) {
+      console.error('Erro ao deletar palavra:', error);
+    }
+  };
+
+ 
+
+
+  return (
+    <Paper elevation={3} sx={{ p: 2 }}>
+      <Typography variant="h6" gutterBottom>
+        Histórico de Traduções
+      </Typography>
+      
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableBody>
+            {Object.entries(history).map(([word, translation]) => (
+              <TableRow key={word} hover sx={{ cursor: 'pointer' }}>
+                <TableCell onClick={() => onSelectWord(word, translation)} component="th" scope="row">
+                  {word}
+                </TableCell>
+                <TableCell align="right">
+                  <IconButton onClick={() => handleDeleteWord(word)}>
+                    <DeleteIcon color="error" />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
+}
